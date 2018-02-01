362869cfcfa91517fdd370e88a3e20e387f0ca39
nicksoule <nicksoule@live.com>
2018-02-01 16:00:30 -0500

    2nd commit with comments

--- src/CarApp.java
+++ src/CarApp.java
@@ -1,3 +1,10 @@
+/*
+ * Nicholas Soule
+ * Lab9 - Java - prompts user for info about any number of cars and store the info in an 
+ * arraylist which can be recalled and printed out
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,7 +12,6 @@ public class CarApp {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
 		String make;
 		String model;
 		int year;
@@ -13,11 +19,13 @@ public class CarApp {
 		int numCars;
 
 		System.out.println("Welcome to the Grand Circus Motors Admin console!");
+		// prompts user to enter a number and validates that it's actually a number
 		numCars = Validator.getInt(scan, "How many cars are you entering? ");
-		
 
+		// arraylist created to store the info for the cars that are about to be entered
 		ArrayList<Car> carList = new ArrayList<>();
 		for (int i = 0; i < numCars; ++i) {
+			// for each car, the program loops through collecting the required date
 			Car carInfo = new Car();
 			System.out.println("Enter Car #" + (i + 1) + " Make: ");
 			make = scan.nextLine();
@@ -25,8 +33,8 @@ public class CarApp {
 			System.out.println("Enter Car #" + (i + 1) + " Model: ");
 			model = scan.nextLine();
 			carInfo.setModel(model);
-			System.out.println("Enter Car #" + (i + 1) + " Year: ");
-			year = scan.nextInt();
+			// validates user entered only valid years for car year
+			year = Validator.getInt(scan, "Enter Car #" + (i + 1) + " Year: ", 1885, 2020);
 			carInfo.setYear(year);
 			System.out.println("Enter Car #" + (i + 1) + " Price: ");
 			price = scan.nextDouble();
@@ -37,13 +45,14 @@ public class CarApp {
 
 		}
 		System.out.println("Current Invetory: ");
-		for(int i = 0; i < carList.size(); i++) {   
-		    String m = carList.get(i).getMake();
-		    String m2 = carList.get(i).getModel();
-		    int y = carList.get(i).getYear();
-		    double p = carList.get(i).getPrice();
-		    System.out.printf("%-10s %-10s %-10s %-10s\n", m , m2, y, "$" + p);
-		}  
+		for (int i = 0; i < carList.size(); i++) {
+			// reassigned values of data to different variables to make the print out statement more readable
+			String m = carList.get(i).getMake();
+			String m2 = carList.get(i).getModel();
+			int y = carList.get(i).getYear();
+			double p = carList.get(i).getPrice();
+			System.out.printf("%-10s %-10s %-10s %-10s\n", m, m2, y, "$" + p);
+		}
 
 	}
 
